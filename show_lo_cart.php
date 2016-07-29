06a82c14b2df9366f1daa1956fb2a985ac6d6d9d
Saif-ur-Rehman <saif54624@yahoo.com>
2016-07-29 10:12:31 +0500

    test4

--- show_lo_cart.php
+++
@@ -1,8 +0,0 @@
-<?php  
-    $opr="";
-    if(isset($_GET['opr']))
-    $opr=$_GET['opr'];
-     if($opr=="chklg"){
-            header('location:login.php');
-        }
-?>
--- strope_test.php
+++
@@ -1,9 +0,0 @@
-require_once('./config.php');
-
-<form class="" action="" method="POST">
-                        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                          data-key="<?php echo $stripe['publishable_key']; ?>"
-                          data-description="Access for a year"
-                          data-amount="5000"
-                          data-locale="auto"></script>
-</form>
